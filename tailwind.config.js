d45722817579112126ee2bd4b8a13e089438cd83
matterisnanto <rachmat.trisnanto186@gmail.com>
2025-02-02 00:42:02 +0700

    perbaikan home page part 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode: 'class',
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
@@ -13,8 +14,44 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                'body': [
+              'Inter', 
+              'ui-sans-serif', 
+              'system-ui', 
+              '-apple-system', 
+              'system-ui', 
+              'Segoe UI', 
+              'Roboto', 
+              'Helvetica Neue', 
+              'Arial', 
+              'Noto Sans', 
+              'sans-serif', 
+              'Apple Color Emoji', 
+              'Segoe UI Emoji', 
+              'Segoe UI Symbol', 
+              'Noto Color Emoji'
+            ],
+                'sans': [
+              'Inter', 
+              'ui-sans-serif', 
+              'system-ui', 
+              '-apple-system', 
+              'system-ui', 
+              'Segoe UI', 
+              'Roboto', 
+              'Helvetica Neue', 
+              'Arial', 
+              'Noto Sans', 
+              'sans-serif', 
+              'Apple Color Emoji', 
+              'Segoe UI Emoji', 
+              'Segoe UI Symbol', 
+              'Noto Color Emoji'
+            ]
             },
+            colors: {
+                primary: {"50":"#fdf2f8","100":"#fce7f3","200":"#fbcfe8","300":"#f9a8d4","400":"#f472b6","500":"#ec4899","600":"#db2777","700":"#be185d","800":"#9d174d","900":"#831843","950":"#500724"}
+              }
         },
     },
     plugins: [
